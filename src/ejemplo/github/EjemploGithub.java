c7e0b56ced4294c8e91960d3ea1847379e34c758
brisanocturna <ramon.soler.hernan@gmail.com>
2017-02-08 10:33:52 +0100

    este es el bueno
    ahora si

--- src/ejemplo/github/EjemploGithub.java
+++ src/ejemplo/github/EjemploGithub.java
@@ -16,8 +16,10 @@ public class EjemploGithub {
      */
     public static void main(String[] args) {
         System.out.println("mi primer control de versiones");
+
         System.out.println("otra linea");
         System.out.println("madre mia otro commit mas");
+
     }
     
 }
